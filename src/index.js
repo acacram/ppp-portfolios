6b0088abf6a9dda457a1169b637f108e180cd544
Alberto <albertocr955@gmail.com>
2024-02-24 17:22:03 +0000

    ADD routing with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+// Aquí es donde van a estar todas las páginas, donde ocurre el redireccionamiento
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </React.StrictMode>
+  </Router>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/login.js
@@ -0,0 +1,12 @@
+// Login
+
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+export default function Login() {
+    return (
+        <div className="App">
+            <Button variant="primary">Login</Button>
+        </div>
+    );
+}
