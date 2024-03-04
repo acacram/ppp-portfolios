f1d4bf1e9389d7bdb1d6e2185563e3ebd5c7d088
Alberto <albertocr955@gmail.com>
2024-03-04 19:07:32 +0000

    CORRECTED environment variables

--- server.js
+++ server.js
@@ -1,44 +1,30 @@
-// Aqui son las rutas del backend
-// app.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const authRoutes = require('./routes/auth'); // Importa las rutas de autenticación
-const registerRoutes = require('./routes/register'); // Importa las rutas de registro
-// Importar las variables de entorno
+const authRoutes = require('./routes/auth');
 require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT_BACK; // Lee el puerto desde las variables de entorno
 
-// Configura el middleware cors
 app.use(cors({
-  origin: 'http://localhost:3001', // Permitir solicitudes solo desde el frontend en localhost:3001
+  origin: 'http://localhost:3001',
 }));
-
-// Middleware para procesar solicitudes JSON
 app.use(bodyParser.json());
-
-// Rutas de autenticación
 app.use('/auth', authRoutes);
-// Rutas de registro de usuario
-app.use('/register', registerRoutes);
 
-// Conexión a la base de datos
-mongoose.connect('mongodb+srv://acaceresr:TYzT8UEtKkleLGgE@cluster0.nvlfzx9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
-// Ruta para obtener todas las cartas
 app.get('/cards', async (req, res) => {
   try {
-    const cards = await Card.find(); // Obtiene todas las cartas desde la base de datos
+    const cards = await Card.find();
     res.json(cards);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
