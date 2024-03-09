523975c62990cf7fe27eecb5cb13fd25de593a0d
Alberto <albertocr955@gmail.com>
2024-03-09 00:45:54 +0000

    IMPROVE alerts create

--- src/components/create.js
+++ src/components/create.js
@@ -29,14 +29,14 @@ import "toastify-js/src/toastify.css";
 const Create = () => {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null); // Cambiado a null
   const [visible, setVisible] = useState(false);
-  const [showTitleAlert, setShowTitleAlert] = useState(false);
-  const [showTextAlert, setShowTextAlert] = useState(false);
-  const [showImageAlert, setShowImageAlert] = useState(false);
+  const [showTitleAlert, setShowTitleAlert] = useState(false); // Estado para controlar la visibilidad de la alerta del título
+  const [showTextAlert, setShowTextAlert] = useState(false); // Estado para controlar la visibilidad de la alerta del texto
+  const [showImageAlert, setShowImageAlert] = useState(false); // Estado para controlar la visibilidad de la alerta de la imagen
   const navigate = useNavigate();
 
-  /**
+/**
    * Maneja la creación de un nuevo elemento.
    *
    * @param {Object} e - Evento de formulario.
@@ -45,32 +45,48 @@ const Create = () => {
   const handleCreate = async (e) => {
     e.preventDefault();
 
+    // Restablece las alertas antes de la validación
+    setShowTitleAlert(false);
+    setShowTextAlert(false);
+    setShowImageAlert(false);
+
+    let isValid = true;
+
     // Validar campos obligatorios individualmente
     if (!title) {
       setShowTitleAlert(true);
-      return;
+      isValid = false;
     }
 
     if (!text) {
       setShowTextAlert(true);
+      isValid = false;
+    }
+
+    // Asumiendo que deseas validar que se haya seleccionado un archivo para la imagen
+    if (!image) {
+      setShowImageAlert(true);
+      isValid = false;
+    }
+
+    // Si algún campo no es válido, detener la ejecución de la función
+    if (!isValid) {
       return;
     }
 
-    if (!image) {
-      setShowImageAlert(true);
-      return;
-    }
+    // Si llegamos aquí, todos los campos son válidos
+    // Continuar con la lógica para enviar los datos...
 
     try {
       const token = localStorage.getItem("token");
       const decodedToken = jwtDecode(token);
       const userId = decodedToken.userId;
 
-      const formData = new FormData();
+      const formData = new FormData(); // Crea un objeto FormData para enviar archivos
       formData.append("title", title);
       formData.append("text", text);
       formData.append("visible", visible);
-      formData.append("image", image);
+      formData.append("image", image); // Agrega la imagen al objeto FormData
       formData.append("userId", userId);
 
       const response = await fetch("http://localhost:5000/cards/createItem", {
@@ -78,11 +94,13 @@ const Create = () => {
         headers: {
           Authorization: `Bearer ${token}`,
         },
-        body: formData,
+        body: formData, // Envía el objeto FormData en lugar de JSON.stringify
       });
 
       if (response.ok) {
         navigate(-1);
+        // const data = await response.json();
+        // console.log(data.message);
 
         Toastify({
           text: "Item creado correctamente",
@@ -114,53 +132,54 @@ const Create = () => {
                 <h3 className="text-center">Crear</h3>
               </Card.Header>
               <Card.Body>
-                {/* Alertas de Bootstrap para cada campo obligatorio */}
-                {showTitleAlert && (
-                  <Alert
-                    variant="danger"
-                    onClose={() => setShowTitleAlert(false)}
-                    dismissible
-                  >
-                    El título es obligatorio.
-                  </Alert>
-                )}
-                {showTextAlert && (
-                  <Alert
-                    variant="danger"
-                    onClose={() => setShowTextAlert(false)}
-                    dismissible
-                  >
-                    El texto es obligatorio.
-                  </Alert>
-                )}
-                {showImageAlert && (
-                  <Alert
-                    variant="danger"
-                    onClose={() => setShowImageAlert(false)}
-                    dismissible
-                  >
-                    La imagen es obligatoria.
-                  </Alert>
-                )}
                 <Form onSubmit={handleCreate}>
+                  {/* Título */}
                   <Form.Group className="mb-3">
                     <Form.Label>Título:</Form.Label>
                     <Form.Control
                       type="text"
-                      placeholder="Enter your title"
+                      placeholder="Introduce el título"
                       value={title}
                       onChange={(e) => setTitle(e.target.value)}
                     />
+                    {showTitleAlert && (
+                      <Alert variant="danger" className="mt-2">
+                        El título es obligatorio.
+                      </Alert>
+                    )}
                   </Form.Group>
+
+                  {/* Texto */}
                   <Form.Group className="mb-3">
                     <Form.Label>Texto:</Form.Label>
                     <Form.Control
                       type="text"
-                      placeholder="Enter your text"
+                      placeholder="Introduce el texto"
                       value={text}
                       onChange={(e) => setText(e.target.value)}
                     />
+                    {showTextAlert && (
+                      <Alert variant="danger" className="mt-2">
+                        El texto es obligatorio.
+                      </Alert>
+                    )}
                   </Form.Group>
+
+                  {/* Imagen */}
+                  <Form.Group className="mb-3">
+                    <Form.Label>Imagen:</Form.Label>
+                    <Form.Control
+                      type="file"
+                      onChange={(e) => setImage(e.target.files[0] ? e.target.files[0] : null)}
+                    />
+                    {showImageAlert && (
+                      <Alert variant="danger" className="mt-2">
+                        La imagen es obligatoria.
+                      </Alert>
+                    )}
+                  </Form.Group>
+
+                  {/* Visibilidad */}
                   <Form.Group className="mb-3">
                     <Form.Check
                       type="checkbox"
@@ -169,13 +188,7 @@ const Create = () => {
                       onChange={(e) => setVisible(e.target.checked)}
                     />
                   </Form.Group>
-                  <Form.Group className="mb-3">
-                    <Form.Label>Imagen:</Form.Label>
-                    <Form.Control
-                      type="file"
-                      onChange={(e) => setImage(e.target.files[0])}
-                    />
-                  </Form.Group>
+
                   <Button type="submit" variant="success" block>
                     Crear
                   </Button>
@@ -188,5 +201,4 @@ const Create = () => {
     </>
   );
 };
-
 export default Create;
